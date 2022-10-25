63563c1ad48c3ac5fae308355a457cea607e8429
David Vaquero <pepesan@gmail.com>
2022-10-25 17:45:50 +0200

    metido ejemplo de rds mysql

--- aws/06_elb/main.tf
+++ aws/06_elb/main.tf
@@ -19,23 +19,6 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  user_data_replace_on_change = true
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  tags = {
-    Name = "terraform-example"
-  }
-}
-
-
 data "aws_vpc" "default" {
   default = true
 }
