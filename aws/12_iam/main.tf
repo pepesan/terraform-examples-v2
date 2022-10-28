b8155aff0c1c8d27627c6ea31e5745f36f11b133
David Vaquero <pepesan@gmail.com>
2022-10-28 18:30:51 +0200

    mejora del ejemplo de manejo de usuarios y políticas asociadas a dichos usuarios

--- aws/12_iam/main.tf
+++ aws/12_iam/main.tf
@@ -9,6 +9,8 @@ resource "aws_iam_user" "demouser" {
   name = "tuckerdemo"
 }
 
+
+
 resource "aws_iam_user" "demo" {
   count = 3
   name = "tuckerdemo.${count.index}"
@@ -56,4 +58,37 @@ output "user_arn" {
 output "user_arn_demo2" {
   value = aws_iam_user.demo2.*.arn
 }
+
+resource "aws_iam_group" "administrators" {
+  name = "Administrators"
+  path = "/"
+}
+
+data "aws_iam_policy" "administrator_access" {
+  name = "AdministratorAccess"
+}
+resource "aws_iam_group_policy_attachment" "administrators" {
+  group      = aws_iam_group.administrators.name
+  policy_arn = data.aws_iam_policy.administrator_access.arn
+}
+
+resource "aws_iam_user" "administrator" {
+  name = "Administrator"
+}
+
+resource "aws_iam_user_group_membership" "devstream" {
+  user   = aws_iam_user.administrator.name
+  groups = [aws_iam_group.administrators.name]
+}
+
+resource "aws_iam_user_login_profile" "administrator" {
+  user                    = aws_iam_user.administrator.name
+  password_reset_required = true
+}
+
+output "password" {
+  value     = aws_iam_user_login_profile.administrator.password
+  #sensitive = true
+}
+
 
