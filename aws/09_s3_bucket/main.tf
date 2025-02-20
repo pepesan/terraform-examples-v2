bf8257c641c286583f4aa2a60d10ec5a92b3f3b6
pepesan <pepesan@gmail.com>
2025-02-20 10:08:51 +0100

    mejoras de los ejemplos de backend s3

--- aws/09_s3_bucket/main.tf
+++ aws/09_s3_bucket/main.tf
@@ -81,16 +81,17 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "${var.project_name}-${var.client_name}-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# ya no es necesaria una tabla de dynamodb para disponer de un backend remoto en s3
+#resource "aws_dynamodb_table" "terraform_locks" {
+#  name         = "${var.project_name}-${var.client_name}-up-and-running-locks"
+#  billing_mode = "PAY_PER_REQUEST"
+#  hash_key     = "LockID"
+#
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#}
 
 
 output "s3_bucket_arn" {
@@ -98,8 +99,9 @@ output "s3_bucket_arn" {
   description = "The ARN of the S3 bucket"
 }
 
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
-}
+# ya no es necesaria una tabla de dynamodb para disponer de un backend remoto en s3
+#output "dynamodb_table_name" {
+#  value       = aws_dynamodb_table.terraform_locks.name
+#  description = "The name of the DynamoDB table"
+#}
 
