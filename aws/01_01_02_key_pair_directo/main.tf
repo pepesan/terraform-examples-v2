fda1c241c150af8cb5669f341190caa2986d961f
David Vaquero <pepesan@gmail.com>
2026-03-20 13:48:33 +0100

    prueba de modificacion para hcp

--- aws/01_01_02_key_pair_directo/main.tf
+++ aws/01_01_02_key_pair_directo/main.tf
@@ -16,23 +16,10 @@ resource "aws_key_pair" "deployer" {
   public_key = var.ssh_public_key
 }
 
-resource "aws_key_pair" "deployer2" {
-  key_name   = "deployer-key-ubuntu-david-test"
-  public_key = var.ssh_public_key
-}
-
 output "ssh_key_name" {
   value = "Clave creada ${aws_key_pair.deployer.key_name}"
 }
 
 output "ssh_arn" {
   value = "ARN creado ${aws_key_pair.deployer.arn}"
-}
-
-output "ssh_key_name2" {
-  value = "Clave creada ${aws_key_pair.deployer2.key_name}"
-}
-
-output "ssh_arn2" {
-  value = "ARN creado ${aws_key_pair.deployer2.arn}"
 }
